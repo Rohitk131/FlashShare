f964777266be6371c0ffcd3eddbb5e299628d1f1
rohitk131 <rohitkumar13103@gmail.com>
2024-10-26 18:58:49 +0530

    added upload

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -10,7 +10,7 @@ export default function FileUploadDemo() {
   };
 
   return (
-    <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
+    <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashedbg-black  border-neutral-800 rounded-lg">
       <FileUpload onChange={handleFileUpload} />
     </div>
   );
